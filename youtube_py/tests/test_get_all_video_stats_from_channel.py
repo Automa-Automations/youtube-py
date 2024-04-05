d4e94a5e37877f33e1e3271d380673a545d4831e
William <willfv2@gmail.com>
2024-04-05 05:52:05 +0200

    fix(test_get_all_video_stats_from_channel): fixed test, now working correctly
    Test is now working correctly, it is running successfully.

--- youtube_py/tests/test_get_all_video_stats_from_channel.py
+++ youtube_py/tests/test_get_all_video_stats_from_channel.py
@@ -1,16 +1,15 @@
 import unittest
-from classes import YoutubeData
+from classes import YoutubeData 
 
 class TestGetAllVideoStatsFromChannel(unittest.TestCase):
 
-    def get_all_video_stats_success(self):
+    def test_get_all_video_stats(self):
 
         youtubeData = YoutubeData()
-        result = youtubeData.get_all_video_stats_from_channel(
-            "@Hamza97"
-        )
+        result = youtubeData.get_all_video_stats_from_channel("@Hamza97")
+
         if result:
             self.assertEqual(result["status"], "success")
             self.assertIsNotNone(result["all_video_stats"])
         else:
-            raise Exception("Getting all video stats failed, result is None")
+            raise Exception("Getting channel ID, result is None")
